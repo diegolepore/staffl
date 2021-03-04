d6376c97cf9bee9ec83e895255a8e4a98915e683
Diego Palacios <diegopalacios3@gmail.com>
2021-03-04 19:53:27 +0100

    Finished main page IU development

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,41 @@
 <template>
-  <ModalWrapper/>
+  <div class="sl-container">
+    <nav class="sl-nav">
+      <div class="sl-logo">
+        <span class="sl-icon--logo"></span>
+      </div>
+    </nav>
+    <div class="sl-info-box">
+      <h1 class="sl-info-box__title">Level Up. Go Stafflist.</h1>
+      <p class="sl-info-box__body">
+        An administration & collaboration tool -
+        Stafflist is the intersectiong point of all the
+        stakeholders in the staffing proccess
+      </p>
+      <div class="sl-info-box__actions">
+        <button class="sl-button">
+          <span class="sl-button__icon sl-icon--bar"></span>
+          Just Click me
+        </button>
+      </div>
+    </div>
 
-  <div class="sl-icon--attention"></div>
-  <div class="sl-icon--close"></div>
-  <div class="sl-icon--date"></div>
-  <div class="sl-icon--email"></div>
-  <div class="sl-icon--info"></div>
-  <div class="sl-icon--plus"></div>
-  <div class="sl-icon--checked"></div>
-  <div class="sl-icon--uncheck"></div>
-  <div class="sl-icon--bar"></div>
+    <div class="sl-abstract-shape">
+      <div class="sl-abstract-shape__sphere"></div>
+      <div class="sl-abstract-shape__stain"></div>
+    </div>
+  </div>
+
+  <!-- <ModalWrapper/> -->
 </template>
 
 <script>
-import ModalWrapper from './components/ModalWrapper.vue'
+// import ModalWrapper from './components/ModalWrapper.vue'
 
 export default {
   name: 'App',
   components: {
-    ModalWrapper
+    // ModalWrapper
   }
 }
 </script>
