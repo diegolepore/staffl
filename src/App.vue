fba229353064d0b4d279800ba6671b563fb9ff05
Diego Palacios <diegopalacios3@gmail.com>
2021-03-04 17:36:12 +0100

    Setup project by creating components, adding assets and base styles

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ModalWrapper/>
+
+  <div class="sl-icon--attention"></div>
+  <div class="sl-icon--close"></div>
+  <div class="sl-icon--date"></div>
+  <div class="sl-icon--email"></div>
+  <div class="sl-icon--info"></div>
+  <div class="sl-icon--plus"></div>
+  <div class="sl-icon--checked"></div>
+  <div class="sl-icon--uncheck"></div>
+  <div class="sl-icon--bar"></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ModalWrapper from './components/ModalWrapper.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ModalWrapper
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import 'assets/styles/main';
 </style>
